947c8a5f80d53f8c643e2ac66a2014a009ec01d0
Eduardo Parracho <efspo@iscte-iul.pt>
2026-03-05 22:45:55 +0000

    Remove CSS styles from script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-body{
-font-family: Arial;
-max-width:500px;
-margin:auto;
-padding:20px;
-}
 
-input,select,button{
-display:block;
-margin:10px 0;
-padding:8px;
-}
-
-button{
-background:#2c7be5;
-color:white;
-border:none;
-border-radius:5px;
-}
